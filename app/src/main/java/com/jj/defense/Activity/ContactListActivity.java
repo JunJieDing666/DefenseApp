c06508559e43b4c5b04217548c688371aefacd7c
Djj <djj4288@126.com>
2016-07-20 23:10:31 +0800

    Hard to add contacts with original code to my real phone.

---
+++ app/src/main/java/com/jj/defense/Activity/ContactListActivity.java
@@ -0,0 +1,150 @@
+package com.jj.defense.Activity;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.jj.defense.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/7/19.
+ */
+public class ContactListActivity extends Activity {
+    private ListView lv_contact = null;
+    //用哈希表做List的成员
+    private List<HashMap<String, String>> contactList = new ArrayList<HashMap<String, String>>();
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            //8.填充数据适配器
+            MyAdapter myAdapter = new MyAdapter();
+            lv_contact.setAdapter(myAdapter);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+
+        initUI();
+        initData();
+    }
+
+    /**
+     * 获得系统联系人数据的方法（耗时操作应放到子线程中进行）
+     */
+    private void initData() {
+        new Thread() {
+            @Override
+            public void run() {
+               /* //1.获得内容解析器
+                ContentResolver contentResolver = getContentResolver();
+                //2.查询系统联系人数据库表的过程（先找到每个联系人对应的id）
+                Cursor cursor = contentResolver.query(Uri.parse("content://com.android.contacts/raw_contacts"),
+                        new String[]{"_id"}, null, null, null);
+                contactList.clear();   //每次列表做添加前都需要清空，避免重复
+                //3.做循环，知道没有游标为止
+                while (cursor.moveToNext()) {
+                    String id = cursor.getString(0);
+                    Log.w("message",id);
+                    //4.用户根据唯一性id找到对应联系人的姓名和电话
+                    Cursor indexCursor = contentResolver.query(Uri.parse("content://com.android.contacts/data"),
+                            new String[]{"data1", "mimetype"},
+                            "raw_contact_id = ? ",
+                            new String[]{id}, null);
+                    //5.做循环，将每个联系人的姓名和电话遍历出来,并将其存放在哈希键值对中
+                    HashMap<String, String> hashMap = new HashMap<String, String>();
+                    while (indexCursor.moveToNext()) {
+                        String data = indexCursor.getString(0);
+                        String type = indexCursor.getString(1);
+                        //Log.w("message",data);
+                        //Log.w("message",type);
+                        //6.区分类型给哈希表做填充
+                        if (type.equals("vnd.android.cursor.item/name")) {
+                            if (!hashMap.isEmpty()) {
+                                hashMap.put("name", data);
+                            }
+                        } else if (type.equals("vnd.android.cursor.item/phone_v2")) {
+                            if (!hashMap.isEmpty()) {
+                                hashMap.put("phone", data);
+                            }
+                        }
+                    }
+                    indexCursor.close();
+                    //得到n个哈希表，每个哈希表都有"name"和"phone"两个键
+                    contactList.add(hashMap);
+                }
+                cursor.close();*/
+
+                //封装好的方法
+                Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null, null, null, null);
+                contactList.clear();
+                while (cursor.moveToNext()) {
+                    String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    //Log.w("message", displayName);
+                    HashMap<String, String> hashMap = new HashMap<String, String>();
+                    hashMap.put("name",displayName);
+                    hashMap.put("phone", number);
+                    contactList.add(hashMap);
+                }
+                cursor.close();
+
+                //7.消息机制，控制UI
+                mHandler.sendEmptyMessage(0);
+
+            }
+        }.start();
+    }
+
+    private void initUI() {
+        lv_contact = (ListView) findViewById(R.id.lv_contact);
+    }
+
+    class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return contactList.size();
+        }
+
+        @Override
+        public HashMap<String, String> getItem(int position) {
+            return contactList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = View.inflate(getApplicationContext(), R.layout.listview_contact_item, null);
+
+            TextView tv_name = (TextView) view.findViewById(R.id.tv_name);
+            TextView tv_phone = (TextView) view.findViewById(R.id.tv_phone);
+
+            tv_name.setText(contactList.get(position).get("name"));
+            tv_phone.setText(contactList.get(position).get("phone"));
+
+            return view;
+        }
+    }
+}
--- app/src/main/java/com/jj/defense/Activity/Setup3Activity.java
+++ app/src/main/java/com/jj/defense/Activity/Setup3Activity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.jj.defense.R;
 
@@ -11,10 +13,36 @@ import com.jj.defense.R;
  * Created by Administrator on 2016/7/19.
  */
 public class Setup3Activity extends Activity {
+    private EditText et_phone_num = null;
+    private Button btn_select_num = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup3);
+
+        initUI();
+    }
+
+    private void initUI() {
+        //显示电话号码的输入框
+        et_phone_num = (EditText) findViewById(R.id.et_phone_num);
+        //点击选择联系人的按钮
+        btn_select_num = (Button) findViewById(R.id.btn_select_num);
+
+        btn_select_num.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ContactListActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //处理返回该界面时带回的结果
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void prePage(View view) {
