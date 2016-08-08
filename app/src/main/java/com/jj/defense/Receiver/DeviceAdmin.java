bf39eb3ed986f0efef4a04c3cb3cd5dc9a14d7a4
Djj <djj4288@126.com>
2016-08-08 22:22:16 +0800

    Accomplish the first function and waiting to test.

---
+++ app/src/main/java/com/jj/defense/Receiver/DeviceAdmin.java
@@ -0,0 +1,15 @@
+package com.jj.defense.Receiver;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Administrator on 2016/8/8.
+ */
+public class DeviceAdmin extends DeviceAdminReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+}
--- app/src/main/java/com/jj/defense/Receiver/SMSReceiver.java
+++ app/src/main/java/com/jj/defense/Receiver/SMSReceiver.java
@@ -1,10 +1,13 @@
 package com.jj.defense.Receiver;
 
+import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
 
 import com.jj.defense.R;
 import com.jj.defense.Service.LoctionService;
@@ -15,6 +18,11 @@ import com.jj.defense.Utils.SpUtils;
  * Created by Administrator on 2016/7/28.
  */
 public class SMSReceiver extends BroadcastReceiver {
+    //获得设备政策管理者
+    private DevicePolicyManager mDPM = null;
+    //判断是否激活权限所需要的组件名
+    private ComponentName mDeviceAdminSample = null;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         //1.判断是否开启了防盗保护
@@ -32,21 +40,49 @@ public class SMSReceiver extends BroadcastReceiver {
                 String messageBody = smsMessage.getMessageBody();
 
                 //7.判断该条短信是否包含播放报警音乐的信息
-                if (messageBody.contains("#*alarm*#")){
+                if (messageBody.contains("#*alarm*#")) {
                     //8.播放音乐
                     MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.ylzs);
                     mediaPlayer.setLooping(true);
-                    mediaPlayer.setVolume(100,100);
+                    mediaPlayer.setVolume(100, 100);
                     mediaPlayer.start();
                 }
 
                 //1.判断短信是否包含获取经纬度坐标的信息
-                if(messageBody.contains("#*location*#")){
+                if (messageBody.contains("#*location*#")) {
                     //2.开启服务来获取坐标
                     context.startService(new Intent(context, LoctionService.class));
                 }
+
+                mDPM = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+                mDeviceAdminSample = new ComponentName(context, DeviceAdmin.class);
+                //锁定屏幕，且可以更改锁屏密码
+                if (messageBody.contains("#*lockscreen*#")) {
+                    if (mDPM.isAdminActive(mDeviceAdminSample)) {
+                        //锁定屏幕
+                        mDPM.lockNow();
+                        //设置锁屏密码
+                        mDPM.resetPassword("", 0);
+                    } else {
+                        Toast.makeText(context, "请激活设备管理器(进入设置-安全-设备管理器勾选)", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                //清除手机数据，还可清除sd卡数据
+                if (messageBody.contains("#*wipedata*#")) {
+                    if (mDPM.isAdminActive(mDeviceAdminSample)) {
+                        //清除手机本机数据
+                        mDPM.wipeData(0);
+                        //清除手机sd卡数据
+                        //mDPM.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
+                    } else {
+                        Toast.makeText(context, "请激活设备管理器(进入设置-安全-设备管理器勾选)", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
             }
         }
-        //
+
     }
+
 }
